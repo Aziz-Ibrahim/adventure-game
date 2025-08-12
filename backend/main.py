66e176fb64b7974fe7e1060406d35d79b5c3944b
Aziz-Ibrahim <dsnxc622@students.codeinstitute.net>
2025-08-12 14:22:48 +0100

    build; Added main app configurations allowing hosts, methods and CORS middleware

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,25 @@
-def main():
-    print("Hello from backend!")
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI(
+    title="Interactive Adventure Game API",
+    description="API for managing and playing the interactive adventure game.",
+    version="1.0.0",
+    docs_url="/docs",
+    redoc_url="/redoc",
+)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 if __name__ == "__main__":
-    main()
+    import uvicorn
+    uvicorn.run(app="main:app", host="0.0.0.0", port=8000, reload=True)
