cde771510c078757a20034c1d384d7932fb7f114
Aziz-Ibrahim <dsnxc622@students.codeinstitute.net>
2025-08-12 20:07:25 +0100

    feat; Added routes to main.py

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from core.config import settings
+from routers import story, job
 
 
 app = FastAPI(
@@ -21,6 +22,8 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+app.include_router(story.router, prefix=settings.API_PREFIX)
+app.include_router(job.router, prefix=settings.API_PREFIX)
 
 if __name__ == "__main__":
     import uvicorn
